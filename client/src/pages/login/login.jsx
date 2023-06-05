9cc5c49eab257ae84caa2b4938f017312fbc0044
priyanka1565 <priyankaingle250@gmail.com>
2023-06-06 00:14:37 +0530

    validation added

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import "./login.css"
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [formErrors, setFormErrors] = useState({});
   // const [forPassword, fortPassword] = useState("");
+
+  //form validation 
+     
+   
+ 
   let arr = [];
   const handleSubmit = (event) => {
-   event.preventDefault();
+    event.preventDefault();
+    if (!email) {
+      toast("Please Enter valid email");
+    }
+    if (!password) {
+      toast("Please Enter valid password ");
+    }
     console.log(email, password);
     let user_info = {
       email: email,
       password: password
     }
     arr.push(user_info);
-    //console.log(arr,"arr")
+    console.log(arr, "arr")
     
   }
+
+  
+ 
 
   return (
     <div className="box">
@@ -23,12 +41,21 @@ const Login = () => {
       <form action="" onSubmit={handleSubmit}>
         <h2>Sign in</h2>
         <div className="inputBox">
-          <input type="text"  onChange={(e)=>setEmail(e.target.value)}/>
+          <input
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          {formErrors.email && <div>{formErrors.email}</div>}
           <span>Email</span>
           <i></i>
         </div>
         <div className="inputBox">
-          <input type="text" onChange={(e)=>setPassword(e.target.value)}/>
+          <input
+            type="text"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
           <span>Password</span>
           <i></i>
         </div>
@@ -38,6 +65,7 @@ const Login = () => {
         </div>
         <input type="submit" />
       </form>
+      <ToastContainer/>
     </div>
   );
 }
