90f21217be4ac1ee0c52fe5a5260dc0da1bf5bc8
priyanka1565 <priyankaingle250@gmail.com>
2023-06-05 22:37:25 +0530

    added

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -8,9 +8,8 @@ import axios from "axios";
 
 export default function Login() {
   const [state, setState] = useState({
-    name: "",
+    username: "",
     password: "",
-    
   });
 
   const handleChange = (filledName) => (e) => {
@@ -39,103 +38,101 @@ export default function Login() {
     
   }
 return (
-    <div className="hold-transition login-page bg-img">
-    
-      <div className="login-box">
-        <div className="login-logo">
-          <img
-            src="../../dist/img/tourdubai/logofull.png"
-            alt="Tourdubailogo"
-            className="logo"
-          />
-        </div>
-        {/* /.login-logo */}
-        <div className="card">
-          <div className="card-body login-card-body">
-            
-              <div>
-                <div className="alert alert-danger alert-dismissible">
-                  <button
-                    type="button"
-                    className="close"
-                    data-dismiss="alert"
-                    aria-hidden="true"
-                  >
-                    ×
-                  </button>
-                  {/* {errorMessage} */}
-                </div>
-              </div>
-            
-            <form onSubmit={handleSubmit} autoComplete="off">
+  <div className="hold-transition login-page bg-img">
+    <div className="login-box">
+      <div className="login-logo">
+        <img
+          src="../../dist/img/tourdubai/logofull.png"
+          alt="Tourdubailogo"
+          className="logo"
+        />
+      </div>
+      {/* /.login-logo */}
+      <div className="card">
+        <div className="card-body login-card-body">
+          <div>
+            <div className="alert alert-danger alert-dismissible">
+              <button
+                type="button"
+                className="close"
+                data-dismiss="alert"
+                aria-hidden="true"
+              >
+                ×
+              </button>
+              {/* {errorMessage} */}
+            </div>
+          </div>
 
-              <div className="input-group mb-3">
-                <input
-                  type="text"
-                 
-                  className={`${''?.username?.length !== 0 ? 'form-control is-invalid' : 'form-control'}`}
+          <form onSubmit={handleSubmit} autoComplete="off">
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className={`${
+                  ""?.username?.length !== 0
+                    ? "form-control is-invalid"
+                    : "form-control"
+                }`}
                 placeholder="Username"
-                value={state.name}
-                 onChange={handleChange("name")}
-                />
+                value={state.username}
+                onChange={handleChange("username")}
+              />
 
-                <div className="input-group-append">
-                  <div className="input-group-text ">
-                    <span className="fas fa-user" />
-                  </div>
+              <div className="input-group-append">
+                <div className="input-group-text ">
+                  <span className="fas fa-user" />
                 </div>
-                {''?.username?.length !== 0 &&
-                  <span id="exampleInputEmail1-error" className="error invalid-feedback">
-                    
-                    {
-                      ''?.username?.map(e=>{
-                        return e
-                      })
-                    }
-                  </span>
-                }
-
               </div>
-              <div className="input-group mb-3">
-                <input
-                  type="password"
-                 className={`${''?.password?.length !== 0 ? 'form-control is-invalid' : 'form-control'}`}
+              {""?.username?.length !== 0 && (
+                <span
+                  id="exampleInputEmail1-error"
+                  className="error invalid-feedback"
+                >
+                 
+                </span>
+              )}
+            </div>
+            <div className="input-group mb-3">
+              <input
+                type="password"
+                className={`${
+                  ""?.password?.length !== 0
+                    ? "form-control is-invalid"
+                    : "form-control"
+                }`}
                 placeholder="Password"
                 value={state.password}
-                 onChange={handleChange("password")}
-                />
-                <div className="input-group-append">
-                  <div className="input-group-text">
-                    <span className="fas fa-lock" />
-                  </div>
+                onChange={handleChange("password")}
+              />
+              <div className="input-group-append">
+                <div className="input-group-text">
+                  <span className="fas fa-lock" />
                 </div>
-                {''?.password?.length !== 0 &&
-                  <span className="error invalid-feedback">
-                    
-                    {
-                      ''?.password?.map(e=>{
-                        return e
-                      })
-                    }
-                  </span>
-                }
               </div>
-              <div className="row login-row">
-                {/* /.col */}
-                <div className="col-4 ">
-                  <button type="submit"  className="btn btn-primary btn-block">
-                    Sign In
-                  </button>
-                </div>
-                {/* /.col */}
+              {""?.password?.length !== 0 && (
+                <span className="error invalid-feedback">
+                  {""?.password?.map((e) => {
+                    return e;
+                  })}
+                </span>
+              )}
+            </div>
+            <div className="row login-row">
+              {/* /.col */}
+              <div className="col-4 ">
+                <button type="submit" className="btn btn-primary btn-block">
+                  Sign In
+                </button>
               </div>
-            </form>
-          </div>
-          {/* /.login-card-body */}
+              {/* /.col */}
+            </div>
+          </form>
         </div>
+        {/* /.login-card-body */}
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 
