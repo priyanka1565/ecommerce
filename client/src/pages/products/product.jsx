0c4be44c1e3946c7e552f30c3dba12ebfe2c4f80
priyanka1565 <priyankaingle250@gmail.com>
2023-06-09 18:24:44 +0530

    added

--- client/src/pages/products/product.jsx
+++ client/src/pages/products/product.jsx
@@ -6,11 +6,12 @@ const Product = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("https://fakestoreapi.com/products");
+                const response = await fetch("https://dummyjson.com/products");
                 const data = await response.json();
                 //console.log(data)
+                let products_data = data.products;
                 setData(data);
-                console.log(data)
+                
                 
             } catch (error) {
                console.error("Error fetching data:", error);
@@ -21,20 +22,18 @@ const Product = () => {
     },[])
 
   return (
-    <div className='product_main'>
-          {data.map((data) => {
-              return (
-                  <div className='product_middle'>
-                <img src={ data.image} alt="" />
-                      <h7>{data.title}</h7>
-                      <h6>{data.category }</h6>
-                      <p>{data.price}</p>
-                      <button>Add to cart</button>
-                  </div>
-               
-              );
-          })}
-        
+    <div className="product_main">
+      {data.products.map((data) => {
+        return (
+          <div className="product_middle">
+            <img src={data.image} alt="" />
+            <h7>{data.title}</h7>
+            <h6>{data.category}</h6>
+            <p>{data.price}</p>
+            <button>Add to cart</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
